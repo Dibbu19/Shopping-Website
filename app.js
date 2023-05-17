904768cc6d6b928ce8764194122f18c9361d6f5d
Divyansh <divyanshchauhan953@gmail.com>
2023-05-18 02:33:57 +0530

    Updating Template

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
-app.set("view engine", "pug");
+// app.set("view engine", "handlebars");
+// app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -17,7 +20,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404" });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
