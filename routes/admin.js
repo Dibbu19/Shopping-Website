dfb75693c77a144627aa8b339b330a0caeb0ac8e
Divyansh <divyanshchauhan953@gmail.com>
2023-06-06 03:05:57 +0530

    Adding More Fields

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-
+const adminController = require("../controllers/admin");
 const rootDir = require("../util/path");
 
-const products = [];
+router.get("/add-product", adminController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get("/products", adminController.getProducts);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", adminController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
